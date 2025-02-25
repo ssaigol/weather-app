bd7b8217b69d46768b517675341d9a05758642fa
Saif Saigol <96741176+ssaigol@users.noreply.github.com>
2025-02-25 18:41:51 -0500

    Update display, update fetch request

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import "./style.css";
+import { format, addDays } from "date-fns";
+import { toZonedTime } from "date-fns-tz";
 
-const input = document.getElementById("search");
-const button = document.getElementById("search-submit");
-const unitInput = document.getElementById("unit");
-const display = document.getElementById("weather");
+const searchInput = document.getElementById("search");
+const searchSubmit = document.getElementById("search-submit");
+const unitSelect = document.getElementById("unit");
+const primary = document.getElementById("primary");
+const cityNameDisp = document.querySelector("#city-name");
+const currentTimeDisp = document.querySelector("#current-time div");
+const lastUpdatedDisp = document.querySelector("#last-updated div");
+const todaysLowDisp = document.querySelector("#todays-low div");
+const currentTempDisp = document.querySelector("#current-temp div");
+const todaysHighDisp = document.querySelector("#todays-high div");
+const descriptionDisp = document.querySelector("#description");
+const feelsLikeDisp = document.querySelector("#feels-like div");
+const currentIconDisp = document.querySelector("#current-icon img");
+
+
+
+
 const imageContext = import.meta.webpackContext("./imgs", {
   recursive: false,
   regExp: /\.(svg)$/,
@@ -14,7 +29,27 @@ imageContext.keys().forEach((file) => {
   images[fileName] = imageContext(file);
 });
 
+const TODAY = format(new Date(), "yyyy-MM-dd");
+const ONEWEEK = format(addDays(TODAY, 8), "yyyy-MM-dd");
+let location = "New York";
+let unitGroup = "us";
+
+
 //Helper Functions
+const searchCity = () => {
+    if (searchInput.value.trim() === "") {
+        renderPrimary();
+    } else {
+        location = searchInput.value;
+        renderPrimary();
+    }
+}
+
+const getTimeinTimeZone = (timezone) => {
+    const zonedTime = toZonedTime(TODAY, timezone);
+    return format(zonedTime, "HH:mm");
+}
+
 const getWindDirection = (degree) => {
   const directions = [
     "N",
@@ -44,106 +79,132 @@ const convertTimeto12H = (string) => {
 };
 
 //API Fetch
-const weather = async (location = "Toronto, Canada", unitGroup) => {
+const weather = async () => {
   const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(location)}?key=KLU5KE9GL7QRF5EPV2FUQESFV&unitGroup=${unitGroup}&iconSet=icons2`,
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(location)}/${TODAY}/${ONEWEEK}?key=KLU5KE9GL7QRF5EPV2FUQESFV&unitGroup=${unitGroup}&lang=en&include=days,hours,current&elements=resolvedAddress,description,datetime,temp,feelslike,icon,tempmin,tempmax,precip,cloudcover,humidity,visibility,windspeedmean,winddir,moonphase,sunrise,sunset,uvindex,precipprob&iconSet=icons2`,
     { mode: "cors" },
   );
   const data = await response.json();
-  console.log(data);
-  const { resolvedAddress, currentConditions, days } = data;
-  console.log(currentConditions);
-  let tempUnit, precipUnit, windUnit, visibilityUnit;
 
-  if (unitGroup === "us") {
-    tempUnit = "\u00B0 F";
-    precipUnit = "inches";
-    windUnit = "mph";
-    visibilityUnit = "miles";
-  }
-
-  if (unitGroup === "metric") {
-    tempUnit = "\u00B0 C";
-    precipUnit = "millimeters";
-    windUnit = "km/h";
-    visibilityUnit = "km";
-  }
-
-  const icon = currentConditions.icon;
-
-  const info = {
-    Conditions: currentConditions.conditions,
-    "Current Temperature": `${currentConditions.temp ?? "N/A"}${tempUnit}`,
-    "Feels Like": `${currentConditions.feelslike ?? "N/A"}${tempUnit}`,
-    "Today's High": `${days[0].tempmax ?? "N/A"}${tempUnit}`,
-    "Today's Low": `${days[0].tempmin ?? "N/A"}${tempUnit}`,
-    Wind: `${days[0].windspeed ?? "N/A"}${windUnit} ${getWindDirection(days[0].winddir ?? 0)}`,
-    Humidity: `${days[0].humidity ?? "N/A"}%`,
-    Precipitation: `${days[0].precip ?? 0} ${precipUnit}`,
-    Visibility: `${currentConditions.visibility ?? "N/A"} ${visibilityUnit}`,
-    "UV Index": currentConditions.uvindex ?? "N/A",
-    Sunrise: `${currentConditions.sunrise.slice(0, 5) ?? "N/A"} AM`,
-    Sunset: `${convertTimeto12H(currentConditions.sunset) ?? "N/A"} PM`,
+  const current = {
+    city: data.resolvedAddress,
+    timezone: data.timezone,
+    lastUpdated: data.currentConditions.datetime,
+    temperature: data.currentConditions.temp,
+    description: data.days[0].description,
+    feelsLike: data.currentConditions.feelslike,
+    icon: data.currentConditions.icon
   };
 
-  return { resolvedAddress, info, icon };
+  const todaysConditions = {
+    todaysLow: data.days[0].tempmin,
+    todaysHigh: data.days[0].tempmax,
+    precip: data.days[0].precip,
+    cloudCover: data.days[0].cloudcover,
+    humidity: data.days[0].humidity,
+    visibility: data.days[0].visibility,
+    windSpeed:  data.days[0].windspeedmean,
+    windDirection: data.days[0].winddir,
+    moonPhase: data.days[0].moonphase,
+    sunrise: data.days[0].sunrise,
+    sunset: data.days[0].sunset,
+    uvIndex: data.days[0].uvindex
+  }
+
+  const hourlyConditions = [];
+  for (let i = 0; i < 24; i++) {
+    const hour = {
+        temp: data.days[0].hours[i].temp,
+        icon: data.days[0].hours[i].icon,
+        precipProb: data.days[0].hours[i].precipprob
+    };
+    hourlyConditions.push(hour);
+  };
+
+  const sevenDayForecast = [];
+  for (let i = 1; i < 8; i++) {
+    const day = {
+        dayLow: data.days[i].tempmin,
+        dayHigh: data.days[i].tempmax,
+        icon: data.days[i].icon,
+        precipProb: data.days[i].precipprob
+    };
+    sevenDayForecast.push(day);
+  }
+
+  console.log(current, todaysConditions, hourlyConditions, sevenDayForecast);
+  return { current, todaysConditions, hourlyConditions, sevenDayForecast };
 };
 
-//Event Listeners
-window.addEventListener("load", async () => {
-  renderLoading();
-  const { resolvedAddress, info, icon } = await weather(
-    "Toronto, Canada",
-    "metric",
-  );
-  render(resolvedAddress, info, icon);
-});
 
-button.addEventListener("click", async () => {
-  renderLoading();
-  let location = input.value || "Toronto";
-  let unit = unitInput.value;
-  const { resolvedAddress, info, icon } = await weather(location, unit);
-  render(resolvedAddress, info, icon);
-});
-
-input.addEventListener("keydown", async (e) => {
-  if (e.key === "Enter") {
-    renderLoading();
-    let location = input.value || "Toronto";
-    let unit = unitInput.value;
-    const { resolvedAddress, info, icon } = await weather(location, unit);
-    render(resolvedAddress, info, icon);
-  }
-});
 
 //Render Functions
-const render = (city, info, icon) => {
-  while (display.firstChild) {
-    display.removeChild(display.firstChild);
-  }
-  const cityName = document.createElement("div");
-  cityName.textContent = city;
-  display.append(cityName);
-  for (const key in info) {
-    const cell = document.createElement("div");
-    cell.innerHTML = `<strong>${key}:</strong> ${info[key]}`;
-    display.append(cell);
-  }
-  renderBackground(icon);
-  input.value = "";
-};
+const renderPrimary = async () => {
+    const { current, todaysConditions } = await weather();
+    cityNameDisp.textContent = current.city;
+    currentTimeDisp.textContent = getTimeinTimeZone(current.timezone);
+    lastUpdatedDisp.textContent = current.lastUpdated.slice(0, 5);
+    todaysLowDisp.textContent = todaysConditions.todaysLow;
+    currentTempDisp.textContent = current.temperature;
+    todaysHighDisp.textContent = todaysConditions.todaysHigh;
+    descriptionDisp.textContent = current.description;
+    feelsLikeDisp.textContent = current.feelsLike;
+    // currentIconDisp.src = `url('${images[current.icon]}')`
+}
 
-const renderLoading = () => {
-  while (display.firstChild) {
-    display.removeChild(display.firstChild);
-  }
-  const loadingMessage = document.createElement("div");
-  loadingMessage.textContent = "Loading...";
-  display.append(loadingMessage);
-};
+//Event Listeners
+window.addEventListener("load", searchCity);
 
-const renderBackground = (icon) => {
-  const background = document.getElementById("background");
-  background.style.backgroundImage = `url('${images[icon]}')`;
-};
+searchSubmit.addEventListener("click", searchCity);
+
+searchInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        searchCity();
+    }
+})
+
+unitSelect.addEventListener("change", () => {
+    unitGroup = unitSelect.value;
+    searchCity();
+})
+
+
+// const render = (city, info, icon) => {
+//   while (display.firstChild) {
+//     display.removeChild(display.firstChild);
+//   }
+//   const cityName = document.createElement("div");
+//   cityName.textContent = city;
+//   display.append(cityName);
+//   for (const key in info) {
+//     const cell = document.createElement("div");
+//     cell.innerHTML = `<strong>${key}:</strong> ${info[key]}`;
+//     display.append(cell);
+//   }
+//   renderBackground(icon);
+//   input.value = "";
+// };
+
+// const renderLoading = () => {
+//   while (display.firstChild) {
+//     display.removeChild(display.firstChild);
+//   }
+//   const loadingMessage = document.createElement("div");
+//   loadingMessage.textContent = "Loading...";
+//   display.append(loadingMessage);
+// };
+
+// const renderBackground = (icon) => {
+//   const background = document.getElementById("background");
+//   background.style.backgroundImage = `url('${images[icon]}')`;
+// };
+
+
+// const weather = async() => {
+//     const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Toronto/2025-02-25/2025-03-04?key=KLU5KE9GL7QRF5EPV2FUQESFV&unitGroup=metric&lang=en&include=days,hours,current&elements=datetime,temp,feelslike,icon,tempmin,tempmax,precip,cloudcover,humidivisibility,windspeedmean,moonphase,sunrise,sunset,uvindex,precipprob&iconSet=icons2", {mode: "cors"})
+
+//     const data = await response.json();
+//     console.log(data);
+// }
+
+// weather();
